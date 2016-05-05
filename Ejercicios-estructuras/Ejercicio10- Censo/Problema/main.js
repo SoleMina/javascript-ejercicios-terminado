ed83a8f6fd6d47f52766d74dcce47803188f52f3
SoleMina <karina.pradogutierrez@gmail.com>
2016-05-04 19:24:16 -0500

    terminado

--- Ejercicios-estructuras/Ejercicio10- Censo/Problema/main.js
+++ Ejercicios-estructuras/Ejercicio10- Censo/Problema/main.js
@@ -1,27 +1,35 @@
 // Escribe aquí tu codigo Javascript
 // Escribe aquí tu codigo Javascript
-
-var cont=0;
-var contf=0;
-var contm=0;
-var contme=0;
- do{
- dni=parseInt(prompt('Ingresar numero de documento:(para finalizar el programa ingresar 0)'));
- if(dni===0) console.log('El programa ha terminado')
- else{
- edad=parseInt(prompt('Ingresar edad:'));
- sexo=prompt('Sexo:(F) o (M)');
- switch (sexo) {
-     case 'F':
-         contf++;
-         break;
-    case 'M':
-    if(edad>16 && edad<65){
-        contme++;
-    }
-        contm++;
-        break;
- }
-cont++;}
- }while(dni!=0)
-console.log('Cantidad de personas censadas:'+cont+'\n Cantidad de varones:'+contm+'\n Cantidad de mujeres:'+contf+'\n Cantidad de varones cuya edad varía entre 16 y 65 años:'+contme);
+var total=prompt("Ingrese N° de personas censadas:");
+var suma_f=0;
+var suma_m=0;
+var varones=0;
+for(var i=1; i<=total; i++){
+    var dni =0;
+    var condicion=true;
+    do{
+        dni=prompt("Ingrese N° de DNI de la persona "+i+" : ");
+        if (dni.length==8) condicion=false;
+        else alert("Vuelve a ingresar el número, es incorrecto");
+    } while (condicion);
+    var edad=prompt("Ingrese edad de la persona "+i+" : ");
+    var sexo=prompt("Ingrese el sexo de la persona(femenino o masculino) "+i+" : ").toLowerCase();  
+    alert("N° de DNI: 0"+dni+"\nEdad: "+edad+"\nSexo: "+sexo);
+    document.write("N° de DNI: 0"+dni+"\nEdad: "+edad+"\nSexo: "+sexo);
+    switch (sexo) {
+        case 'femenino':
+            suma_f++;
+            break;
+        case 'masculino':
+            suma_m++;
+            if(edad>=16&&edad<=65){
+                varones++;
+            } 
+            break;
+        default:
+            alert("Escriba correctamente (masculino o femenino)");
+            break;
+    }   
+}
+document.write("La cantidad total de personas censadas es "+total+"\nLa cantidad de varones es "+sum+"\nLa cantidad de mujeres es "+suma+"\nLa cantidad de varones cuya edad varía entre 16 y 65 años es "+varones);
+Conversación de chat finalizada
